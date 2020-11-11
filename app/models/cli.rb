cd5bec3014d1510dd67d7926e26502d780f41845
matthewogtong <ogtongm@gmail.com>
2020-11-11 17:27:02 -0500

    added gameplay logic

--- app/models/cli.rb
+++ app/models/cli.rb
@@ -10,7 +10,7 @@ class CLI
     @@current_game = nil
     @@font = TTY::Font.new(:doom)
     @@table = TTY::Table.new(
-        header: ["⚔","🚪", "⚔"], 
+        header: ["⚔","🚪"], 
         rows: [["⚔", "💰", "⚔"], 
         ["⚔", "⚔", "⚔"],
         ["⚔", "⚔", "⚔"]]
@@ -33,11 +33,11 @@ class CLI
     def self.main_menu
         system('clear')
         self.logo
-        choice = @@prompt.select("Choose an Option") do |prompt|
-            prompt.choice "Log In"
-            prompt.choice "Create User"
-            prompt.choice "Delete User"
-            prompt.choice "Manual"
+        choice = @@prompt.select("Choose an Option") do |p|
+            p.choice "Log In"
+            p.choice "Create User"
+            p.choice "Delete User"
+            p.choice "Manual"
         end
         case choice
         when "Log In"
@@ -54,9 +54,9 @@ class CLI
     def self.login
         system('clear')
         self.logo
-        choice = @@prompt.select("Choose an Option") do |prompt|
-            prompt.choice "Login"
-            prompt.choice "Main Menu"
+        choice = @@prompt.select("Choose an Option") do |p|
+            p.choice "Login"
+            p.choice "Main Menu"
         end 
         case choice
         when "Login"
@@ -81,9 +81,9 @@ class CLI
     def self.create_user
         system('clear')
         self.logo
-        choice = @@prompt.select("Choose an Option") do |prompt|
-            prompt.choice "Create User"
-            prompt.choice "Main Menu"
+        choice = @@prompt.select("Choose an Option") do |p|
+            p.choice "Create User"
+            p.choice "Main Menu" 
         end 
         case choice
         when "Create User"
@@ -102,9 +102,9 @@ class CLI
     def self.delete_user
         system('clear')
         self.logo
-        choice = @@prompt.select("Choose an Option") do |prompt|
-            prompt.choice "Delete User"
-            prompt.choice "Main Menu"
+        choice = @@prompt.select("Choose an Option") do |p|
+            p.choice "Delete User"
+            p.choice "Main Menu"
         end 
         case choice
         when "Delete User"
@@ -176,21 +176,289 @@ class CLI
     end
 
     #Game Components -----------------------------------------------------
-    def self.game_header
-        puts @@font.write("LEVEL 1", letter_spacing: 2)
+    def self.game_header(level)
+        puts @@font.write("#{level}", letter_spacing: 2)
     end
     
-    def self.game_map
+    def self.game_map_first_level
         system('clear')
-        self.game_header
+        self.game_header("LEVEL ONE")
+        puts @@table.render(:ascii, padding: 2)
+    end
+
+    def self.game_map_second_level
+        system('clear')
+        self.game_header("LEVEL TWO")
+        puts @@table.render(:ascii, padding: 2)
+    end
+
+    def self.game_map_third_level
+        system('clear')
+        self.game_header("LEVEL THREE")
         puts @@table.render(:ascii, padding: 2)
     end
     
     def self.game_start
+        system('clear')
+        puts "You are entering Level 1!"
         sleep(2)
         system('clear')
-        self.game_map
+        self.game_map_first_level
+        self.level_first_choice
     end
 
+    def self.proceed_option
+        choice = @@prompt.select("What would you like to do next?") do |p|
+            p.choice "Proceed"
+            p.choice "Check stats"
+            p.choice "Exit to Main Menu (Warning - You will lose all progress)"
+        end
+        case choice
+        when "Proceed"
+            if @@current_game.move_count == 1
+                self.level_second_choice
+            elsif @@current_game.move_count == 2
+                self.level_third_choice
+            elsif @@current_game.move_count == 3
+                self.level_fourth_choice
+            end
+        when "Check stats"
+            puts "Your character has #{@@current_game.character.money} money and #{@@current_game.character.power} power."
+            second_choice = @@prompt.select("What would you like to do next?") do |p|
+                p.choice "Proceed"
+            end
+            case second_choice
+            when "Proceed"
+                if @@current_game.move_count == 1
+                    self.level_second_choice
+                elsif @@current_game.move_count == 2
+                    self.level_third_choice
+                elsif @@current_game.move_count == 3
+                    self.level_fourth_choice
+                end
+            end
+        when "Exit to Main Menu (Warning - You will lose all progress)"
+            system('clear')
+            self.logo
+            puts "You wimped out, your family will not survive the winter."
+            sleep(3)
+            self.main_menu
+        end
+    end
+
+    def self.only_proceed_option
+        choice = @@prompt.select("What would you like to do next?") do |p|
+            p.choice "Proceed"
+        end
+        case choice
+        when "Proceed"
+            if @@current_game.move_count == 1
+                self.level_second_choice
+            elsif @@current_game.move_count == 2
+                self.level_third_choice
+            elsif @@current_game.move_count == 3
+                self.level_fourth_choice
+            end
+        end
+    end
+
+    def self.money_path
+        puts "Along your way you happen to find some money on the ground."
+        puts "Finders keepers, losers weepers."
+        @@current_game.character.money += 4
+        self.proceed_option
+    end
+
+    def self.power_up_path
+        puts "You come across an axe lying on the ground."
+        puts "You grab the axe cause why not. You feel your power grow..."
+        @@current_game.character.power += 2
+        self.proceed_option
+    end
+
+    def self.shop_path
+        
+    end
+    
+    def self.duck_attack_sequence
+        system('clear')
+        #insert art of duck
+        puts "You have #{@@current_game.character.power} power and #{@@current_game.character.hp} hp. "
+        choice = @@prompt.select("") do |p|
+            p.choice "Roll"
+        end
+        case choice 
+        when "Roll"
+            player_roll = rand((@@current_game.character.power)..30)
+            duck_roll = rand(1..30)
+            puts "You rolled a #{player_roll}."
+            puts "The duck rolled a #{duck_roll}."
+            if player_roll > duck_roll
+                @@current_game.character.power += 2
+                puts "Nice, you beat a duck, good for you!"
+                puts "You gained +2 to your power"
+                self.only_proceed_option
+            else
+                @@current_game.character.hp -= 2
+                puts "You lost to a duck..."
+                puts "You lose 2 hp"
+                sleep(3)
+                self.only_proceed_option
+            end
+        end    
+    end
+
+    def self.duck_fight
+        puts "You see a duck. It doesn't seem to be aggresive."
+        choice = @@prompt.select("What would you like to do next?") do |p|
+            p.choice "Proceed"
+            p.choice "Check stats"
+            p.choice "Attack"
+            p.choice "Exit to Main Menu (Warning - You will lose all progress)"
+        end
+        case choice
+        when "Proceed"
+            if @@current_game.move_count == 1
+                self.level_second_choice
+            elsif @@current_game.move_count == 2
+                self.level_third_choice
+            elsif @@current_game.move_count == 3
+                self.level_fourth_choice
+            end
+        when "Check stats"
+            puts "Your character has #{@@current_game.character.money} money and #{@@current_game.character.power} power."
+            second_choice = @@prompt.select("What would you like to do next?") do |p|
+                p.choice "Proceed"
+            end
+            case second_choice
+            when "Proceed"
+                if @@current_game.move_count == 1
+                    self.level_second_choice
+                elsif @@current_game.move_count == 2
+                    self.level_third_choice
+                elsif @@current_game.move_count == 3
+                    self.level_fourth_choice
+                end
+            end
+        when "Attack"
+            puts "You decide to show the duck who's boss."
+            sleep(2)
+            self.duck_attack_sequence
+        when "Exit to Main Menu (Warning - You will lose all progress)"
+            system('clear')
+            self.logo
+            puts "You wimped out, your family will not survive the winter."
+            sleep(3)
+            self.main_menu
+        end
+    end
+
+    def self.boar_fight
+        system('clear')
+        #insert art of boar
+        puts "You see a boar. It seems to be charging at you."
+        puts "Fleeing is not an option."
+        puts "You have #{@@current_game.character.power} power and #{@@current_game.character.hp} hp. "
+        choice = @@prompt.select("") do |p|
+            p.choice "Roll"
+        end
+        case choice 
+        when "Roll"
+            player_roll = rand((@@current_game.character.power)..40)
+            boar_roll = rand(10..30)
+            puts "You rolled a #{player_roll}."
+            puts "The boar a #{boar_roll}."
+            if player_roll > boar_roll
+                @@current_game.character.power += 4
+                puts "Nice, you beat a duck, good for you!"
+                puts "You gained +4 to your power"
+                self.only_proceed_option
+            else
+                @@current_game.character.hp -= 3
+                puts "The boar's tusks tore right through you!"
+                puts "You lose 3 hp"
+                sleep(3)
+                self.only_proceed_option
+            end
+        end   
+        
+    end
+
+    def self.tukery_fight
+        
+    end
+    
+    def game_over
+
+    end
+    #Level Choices -----------------------------------------------------
+
+    def self.level_first_choice
+        #will need a character location corresponding to game map
+        self.game_map_first_level
+        choice = @@prompt.select("Select your first path wisely") do |p|
+            p.choice "Right Path"
+            p.choice "Left Path"
+        end
+        case choice
+        when "Right Path" #move character location to path on board
+            @@current_game.move_count = 1
+            self.duck_fight #this will be a random space
+        when "Left Path" #move character location to path on board
+            @@current_game.move_count = 1
+            self.power_up_path
+        end
+    end
+
+    def self.level_second_choice
+        #will need a character location corresponding to game map
+        self.game_map_first_level
+        choice = @@prompt.select("Select your second path wisely") do |p|
+            p.choice "Right Path"
+            p.choice "Left Path"
+        end
+        case choice
+        when "Right Path" #move character location to path on board
+            @@current_game.move_count = 2
+            self.money_path
+        when "Left Path" #move character location to path on board
+            @@current_game.move_count = 2
+            self.boar_fight
+        end
+    end
+
+    def self.level_third_choice
+        #will need a character location corresponding to game map
+        self.game_map_first_level
+        choice = @@prompt.select("Select your third path wisely") do |p|
+            p.choice "Right Path"
+            p.choice "Left Path"
+        end
+        case choice
+        when "Right Path" #move character location to path on board
+            @@current_game.move_count = 3
+            self.money_path
+        when "Left Path" #move character location to path on board
+            @@current_game.move_count = 3
+            self.power_path
+        end
+    end
+    
+    def self.level_fourth_choice
+        #will need a character location corresponding to game map
+        self.game_map_first_level
+        choice = @@prompt.select("Select your fourth path wisely") do |p|
+            p.choice "Right Path"
+            p.choice "Left Path"
+        end
+        case choice
+        when "Right Path" #move character location to path on board
+            @@current_game.move_count = 4
+            self.money_path
+        when "Left Path" #move character location to path on board
+            @@current_game.move_count = 4
+            self.power_up_path
+        end
+    end
 
 end #.class CLI
